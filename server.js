0c66e000bf0f2aa43d00ab5b50f538ad4fe78534
Abhiram13 <abhiramnagadi@gmail.com>
2024-05-25 21:09:00 +0530

    nginx, serverjs

---
+++ server.js
@@ -0,0 +1,19 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/Transactions-client/browser'));
+
+app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname + '/dist/Transactions-client/browser/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+try {
+   app.listen(process.env.PORT || 8080);
+   console.log("Angular server is started");
+} catch (e) {
+   console.log("Angular server is not starting", e);
+}
