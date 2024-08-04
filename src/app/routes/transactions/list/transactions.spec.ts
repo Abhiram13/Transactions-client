3c06433e974a5fafcd6d978d909db1d72e2c3f9f
Abhiram13 <abhiramnagadi@gmail.com>
2024-08-04 14:52:39 +0530

    element, api response check

--- src/app/routes/transactions/list/transactions.spec.ts
+++ src/app/routes/transactions/list/transactions.spec.ts
@@ -4,10 +4,11 @@ import { Router } from "@angular/router";
 import { RouterTestingModule } from "@angular/router/testing";
 import { TransactionService } from "../../../services/transactions.service";
 import { HttpService } from "../../../services/http.service";
-import { Inject, Injectable } from "@angular/core";
+import { DebugElement, Inject, Injectable } from "@angular/core";
 import { ComponentService } from "../../../services/common.service";
 import { HttpClientTestingModule } from "@angular/common/http/testing"
 import { HttpClient } from "@angular/common/http";
+import { By } from "@angular/platform-browser";
 
 describe("Transactions list", function() {
     let component: TransactionListComponent;
@@ -29,12 +30,31 @@ describe("Transactions list", function() {
         component = fixture.componentInstance;
         router = TestBed.inject(Router);
         service = TestBed.inject(TransactionService);
+        fixture.detectChanges();
     });
 
     it('should navigate to transactions', () => {
-        expect(component.selectedMonth).toBe("");
-        component.ngOnInit();
         expect(component.selectedMonth).toBe("Aug");
-        component;
+        component.ngOnInit();        
+    });
+
+    it('should be greater than zero', () => {
+        const queryParams = {
+            month: "08",
+            year: "2024"
+        };
+
+        service.list(queryParams).subscribe(res => {
+            expect(component.dataSource.length).toBeGreaterThan(0);
+        });
+    });
+
+    it ("should element be exist", () => {
+        const elements: DebugElement[] = fixture.debugElement.queryAll(By.css("h4"));        
+    
+        elements?.map(element => {
+            const heading: HTMLHeadingElement = element?.nativeElement;
+            expect(heading.textContent).toBeTruthy();
+        });
     });
 });
